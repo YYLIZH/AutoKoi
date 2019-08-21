c190ddd5060c74e339fd5a9f8214a32d58151205
YYLIZH <48501277+YYLIZH@users.noreply.github.com>
2019-08-21 10:14:10 +0800

    Add files via upload

---
+++ AutoKoi.py
@@ -0,0 +1,30 @@
+import pyautogui
+import threading
+#獲取螢幕大小
+#get the size of your screen
+width, height = pyautogui.size()
+print("width is ", width, ", and height is", height)
+#滑鼠指到目標處，獲取目標處位址
+#point the mouse to the target, and get the position of the target
+target = pyautogui.position()
+
+
+def AutoClick():
+#設定每次按按鍵中間間隔
+#set the pause time between every click
+    pyautogui.PAUSE = 20
+    pyautogui.FAILSAFE = True
+
+#在函式中重複呼叫，重複按4組
+#use resursive function to repeat calling the function
+    for i in range(4):
+        for j in range(3):
+            pyautogui.click(target)
+    global timer
+    timer = threading.Timer(3000, AutoClick)
+    timer.start()
+
+#第一次可以設定什麼時候開始
+#set when will the first click
+timer = threading.Timer(1, AutoClick)
+timer.start()
