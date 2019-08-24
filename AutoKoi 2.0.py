ebd63dc2ca2cf549ef6103668ba7297f3a79b650
YYLIZH <48501277+YYLIZH@users.noreply.github.com>
2019-08-24 15:24:12 +0800

    Update AutoKoi 2.0.py
    ver 2.1新增滑鼠點擊間隔為20~30秒之間的亂數

--- AutoKoi 2.0.py
+++ AutoKoi 2.0.py
@@ -1,14 +1,17 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
 import pyautogui
 import time
+import random
+
 now=time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
 target = pyautogui.position()
 print(now)
 
 def AutoClick():
-#設定每次按按鍵中間間隔
+#設定每次按按鍵中間間隔，單位為秒
 #set the pause time between every click
-    pyautogui.PAUSE = 25
+#ver2.1 新增以亂數秒點擊
+    pyautogui.PAUSE = random.randint(20,30)
     pyautogui.FAILSAFE = True
 
 #在函式中重複呼叫，重複按4組
