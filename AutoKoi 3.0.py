4cd81b5378d8063203b459d5a6b6bdb885fd1045
YYLIZH <48501277+YYLIZH@users.noreply.github.com>
2019-08-29 00:33:05 +0800

    Add files via upload

--- AutoKoi 3.0.py
+++ AutoKoi 3.0.py
@@ -3,27 +3,27 @@ import pyautogui
 import time
 import random
 
-now=time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+now = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
 target = pyautogui.position()
 print(now)
-target_pix = pyautogui.screenshot().getpixel((target))
+target_pix = pyautogui.screenshot().getpixel(target)
 
 
 
 def AutoClick():
-#設定每次按按鍵中間間隔
-#set the pause time between every click
-    pyautogui.PAUSE = random.randint(20,30)
+    # 設定每次按按鍵中間間隔
+    # set the pause time between every click
+    pyautogui.PAUSE = random.randint(20, 30)
     pyautogui.FAILSAFE = True
 
-#在函式中重複呼叫，重複按4組
-#use resursive function to repeat calling the function
+    # 在函式中重複呼叫，重複按4組
+    # use recursive function to repeat calling the function
     for i in range(5):
         pyautogui.click(target)
         pyautogui.click(target)
         pyautogui.click(target)
-        #在此檢查RGB 三色，
-        now_pix = pyautogui.screenshot().getpixel((target))
+        # 在此檢查RGB三色，然後跟最早的比對
+        now_pix = pyautogui.screenshot().getpixel(target)
         if now_pix == target_pix:
             pyautogui.click(target)
         else:
@@ -31,9 +31,11 @@ def AutoClick():
         pyautogui.click(target)
 
 
-
+# 設定延時執行的秒數
 time.sleep(2400)
+# 開始執行程式，由於後面sched的interval排程無法執行第一次，所以我們先讓他執行一次
 AutoClick()
 sched = BlockingScheduler()
+# 將自動點擊加入排程
 sched.add_job(AutoClick, 'interval', minutes=50)
 sched.start()
